73134804dbfe0c1f9b0a57ede430036cf7dcc2b6
swlee7208 <swlee7208@gmain.com>
2025-08-21 10:49:16 +0900

    1차 시뮬 완료 버전

--- etc/mount_bitlocker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo dislocker -V /dev/sda1 --user-password=11111111 -- /mnt/bitlocker
-sleep 1
-sudo mount -t exfat -o loop,uid=1000,gid=1000,umask=000 \
-  /mnt/bitlocker/dislocker-file /mnt/bitlocker-unlocked
